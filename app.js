9ccf39a24191e4dbc9e57da8dafe9a14fd21aca0
said mounaim <saidmounaim00@gmail.com>
2020-01-14 15:33:38 +0100

    Fisrt Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+TweenMax.defaultEase = Linear.easeOut;
+const tl = new TimelineMax({ paused: true });
+
+
+$('#pagepiling').pagepiling({
+  menu: null,
+  direction: 'vertical',
+  anchors: [],
+  loopBottom: false,
+  loopTop: false,
+  css3: true,
+  onLeave: (index, nextIndex, direction) => {
+    const Select = $('#'+index + ' span');
+    // tl.staggerTo(Select, 0.5, { y: "60", opacity: 0 });
+  }
+});
